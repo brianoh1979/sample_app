5ee2517d7682fe160ac176764acafbb323b5ed6f
Brian OHalloran <brianoh@circleci.com>
2019-08-19 17:09:45 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "hello, world!" #re-adding after git push mess
   end
 end
