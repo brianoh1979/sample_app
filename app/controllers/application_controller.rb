676d90ff9a00c6ec1d1e23f442ca776922c8b20b
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-04 19:29:19 +0100

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "hello, world!" #re-adding after git push mess
-  end
+  include SessionsHelper
 end
