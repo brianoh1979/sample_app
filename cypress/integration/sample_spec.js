2a68025ffe11671e9987200605d6edc3363715d4
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-11 11:23:23 +0100

    Amending sample_spec to use sample test from documentation to ensure green build

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,11 +1,5 @@
-describe('The Home Page', function() {
-  it('successfully loads', function() {
-<<<<<<< HEAD
-    cy.visit('http://0.0.0.0:3000/login') // change URL to match your dev URL
-||||||| merged common ancestors
-    cy.visit('http://localhost:3000') // change URL to match your dev URL
-=======
-    cy.visit('http://0.0.0.0:3000') // change URL to match your dev URL
->>>>>>> bc61db9e0afb18e5cd65bb56b06e011241b73e51
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(true)
   })
 })
---
+++ cypress/integration/sample_spec_browsetest.js
@@ -0,0 +1,5 @@
+describe('The Home Page', function() {
+  it('successfully loads', function() {
+    cy.visit('http://0.0.0.0:3000') // change URL to match your dev URL
+  })
+})
