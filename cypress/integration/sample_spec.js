6eab2a004ad0090c000f13b929156e4c657e674a
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-13 15:55:31 +0100

    Changed cy.visit to cy.request

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
 describe('The signin page', function() {
   it('successfully loads', function() {
-    cy.visit('http://0.0.0.0:3000/login') // change URL to match your dev URL
+    cy.request('http://0.0.0.0:3000') // change URL to match your dev URL
   })
 })
