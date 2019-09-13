fe42780776b5cc7d22ba1212a61a0d889624fac6
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-13 14:21:29 +0100

    Amended sample_spec.js in Cypress Integration tests to use website default

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true)
+describe('The signin page', function() {
+  it('successfully loads', function() {
+    cy.visit('http://localhost:3000') // change URL to match your dev URL
   })
 })
