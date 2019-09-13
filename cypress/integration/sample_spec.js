d33a55e1a608daf8987d808876b376234c7edd7d
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-13 15:01:58 +0100

    Amended sample_spec.js back to using http and added login page

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
 describe('The signin page', function() {
   it('successfully loads', function() {
-    cy.visit('tcp://localhost:3000') // change URL to match your dev URL
+    cy.visit('http://localhost:3000/login') // change URL to match your dev URL
   })
 })
