b3461e447a517b439ec8a49ce0fb2a6a5586653f
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-17 17:55:58 +0100

    Splitting out user tests into seperate files and archiving layout test

--- spec/features/models/user2_spec.rb
+++ spec/features/models/user2_spec.rb
@@ -9,19 +9,19 @@ describe User do
 
   subject { @user }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
+#  it { should respond_to(:name) }
+#  it { should respond_to(:email) }
+#  it { should respond_to(:password_digest) }
+#  it { should respond_to(:password) }
+#  it { should respond_to(:password_confirmation) }
+#  it { should respond_to(:authenticate) }
 
-  it { should be_valid }
+#  it { should be_valid }
 
-#  describe "when name is not present" do
-#    before { @user.name = " " }
-#    it { should_not be_valid }
-#  end
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 
 #  describe "when email is not present" do
 #    before { @user.email = " " }
