6dd4a9a51beaae3b488246b294894cbcd73c3a03
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-17 16:23:06 +0100

    Amending user_spec.rb to add in extra end step and indent do statements

--- spec/features/models/user_spec.rb
+++ spec/features/models/user_spec.rb
@@ -2,33 +2,34 @@ require 'spec_helper'
 
 describe User do
 
-before do
-  @user = User.new(name: "Example User", email: "user@example.com",
-                  password: "foobar", password_confirmation: "foobar")
-end
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                    password: "foobar", password_confirmation: "foobar")
+  end
 
-subject { @user }
+  subject { @user }
 
-it { should respond_to(:name) }
-it { should respond_to(:email) }
-it { should respond_to(:password_digest) }
-it { should respond_to(:password) }
-it { should respond_to(:password_confirmation) }
-it { should respond_to(:authenticate) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
-it { should be_valid }
+  it { should be_valid }
 
-describe "when name is not present" do
-  before { @user.name = " " }
-  it { should_not be_valid }
-end
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 
-describe "when email is not present" do
-  before { @user.email = " " }
-  it { should_not be_valid }
-end
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
 
-describe "when name is too long" do
-  before { @user.name = "a" * 51 }
-  it { should_not be_valid }
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
 end
