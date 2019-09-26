4808ecd43d93724dd7d95f9b72b2990f4c18b954
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-26 20:31:41 +0100

    Amending of deploy-heroku.sh

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -1,6 +1,6 @@
 set -e
 set -u
-source ./setup-env.sh
-echo "Pushing branch ${CIRCLE_BRANCH} to app ${HEROKU_APP}"
-git remote add heroku https://git.heroku.com/${HEROKU_APP}.git
-git push heroku ${CIRCLE_BRANCH}:master
+#source ./setup-env.sh
+echo "Pushing branch Develop to app thawing-cliffs-50633"
+#git remote add heroku https://git.heroku.com/thawing-cliffs-50633.git
+git push heroku develop
