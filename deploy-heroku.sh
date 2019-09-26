84f8aa89cb683872c21d7173266d8fbe3435603e
Brian O'Halloran <brian_o_halloran@hotmail.com>
2019-09-26 15:49:09 +0100

    Recreating the shell scripts for the application

---
+++ deploy-heroku.sh
@@ -0,0 +1,6 @@
+set -e
+set -u
+source ./setup-env.sh
+echo "Pushing branch ${CIRCLE_BRANCH} to app ${HEROKU_APP}"
+git remote add heroku https://git.heroku.com/${HEROKU_APP}.git
+git push heroku ${CIRCLE_BRANCH}:master
---
+++ login-heroku.sh
@@ -0,0 +1,13 @@
+cat > ~/.netrc << EOF
+    machine api.heroku.com
+        login "brianoh@circlici.com"
+        password "1abed4d8-766e-4c04-8132-fdddc186695a"
+    machine git.heroku.com
+        login "brianoh@circleci.com"
+        password "1abed4d8-766e-4c04-8132-fdddc186695a"
+EOF
+
+# Add heroku.com to the list of known hosts
+#mkdir ~/.ssh
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+
