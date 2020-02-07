a2e4534ee24b8545c3590ac229f316f19cff14db
Brian O'Halloran <brian_o_halloran@hotmail.com>
2020-02-07 17:49:27 +0000

    Recreating job to test custom Junit code appearing in test summary and amending .rb rspec file to contain custom code

--- spec/features/models/user_spec.rb
+++ spec/features/models/user_spec.rb
@@ -18,6 +18,41 @@ describe User do
 
   it { should be_valid }
 
+string = <<EOF
+
+<?xml version="1.0" encoding="ISO-8859-15"?>
+
+<!DOCTYPE bibliography PUBLIC "-//OASIS//DTD DocBook XML V4.2//EN"
+
+    "http://www.oasis-open.org/docbook/xml/4.2/docbookx.dtd">
+
+<bibliography>
+
+    <biblioentry id="FHIW13C-1234">
+
+      <author>
+
+        <firstname>Godfrey</firstname>
+
+        <surname>Vesey</surname>
+
+      </author>
+
+      <title>Personal Identity: A Philosophical Analysis</title>
+
+      <publisher>
+
+        <publishername>Cornell University Press</publishername>
+
+      </publisher>
+
+      <pubdate>1977</pubdate>
+
+   </biblioentry>
+
+</bibliography>
+
+EOF
 #  describe "when name is not present" do
 #    before { @user.name = " " }
 #    it { should_not be_valid }
